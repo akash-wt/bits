09bc354988802f7e75f086b63e6aa81a6be65d64
akash patel <akash906129@gmail.com>
2026-02-14 16:45:54 +0530

    fixed sign_in..

--- app/App.tsx
+++ app/App.tsx
@@ -1,49 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
-import { signInNonce } from "./solana/connectWallet";
+import { connectWallet, signInNonce } from "./solana/connectWallet";
+import { useState } from "react";
 
 export default function App() {
-  const SIGN_IN_PAYLOAD = {
-    domain: "yourdomain.com",
-    nonce: "12sjdb784",
-    statement: "Sign into React Native Sample App",
-    uri: "https://bits.app",
-  };
-
-  const SIGN_IN_RESULT = {
-    accounts: [
-      {
-        address: "gObY6t4/1KRzDjE8AhuzgkqRNYYhz0HSG707I2SEBCo=",
-        label: "phantom-wallet",
-      },
-    ],
-    auth_token:
-      "eyJ0eXAiOiJwaGFudG9tLXdhbGxldC1hdXRoLXRva2VuIiwiaWlkIjoiMSIsInRpZCI6IjIifTf5fORlaneuQkWzkxWr34rvAaI53VFBLtJp0zIieRSC",
-    sign_in_result: {
-      address: "gObY6t4/1KRzDjE8AhuzgkqRNYYhz0HSG707I2SEBCo=",
-      signature:
-        "px1wlarjFmFq23BLMntN3KyLlAgMfSxeIOXruAlQkucMASdcMx8QjOUBjR7gD64NFto3Nnax5YORFDi0sA1GAA==",
-      signed_message:
-        "eW91cmRvbWFpbi5jb20gd2FudHMgeW91IHRvIHNpZ24gaW4gd2l0aCB5b3VyIFNvbGFuYSBhY2NvdW50Ogo5Z0JNQkRtdUREVVRTZGdpUTFTRzZ3RVprUDRYNUhGVnRZRFI1Q015dkZFeQoKU2lnbiBpbnRvIFJlYWN0IE5hdGl2ZSBTYW1wbGUgQXBwCgpVUkk6IGh0dHBzOi8vYml0cy5hcHAKTm9uY2U6IDEyc2pkYjc4NA",
-    },
-  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start on your app!</Text>
-      {/* <Button
-        title="verfiy wallet"
-        onPress={verifySIWS(SIGN_IN_PAYLOAD,SIGN_IN_RESULT)}
-      ></Button> */}
+      <Button title="verfiy wallet" onPress={connectWallet}></Button>
       <Button title="signin nonce" onPress={signInNonce}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-/*
-sign in nonce result {"accounts": [{"address": "gObY6t4/1KRzDjE8AhuzgkqRNYYhz0HSG707I2SEBCo=", "label": "phantom-wallet"}], "auth_token": "eyJ0eXAiOiJwaGFudG9tLXdhbGxldC1hdXRoLXRva2VuIiwiaWlkIjoiMSIsInRpZCI6IjIifTf5fORlaneuQkWzkxWr34rvAaI53VFBLtJp0zIieRSC", "sign_in_result": {"address": "gObY6t4/1KRzDjE8AhuzgkqRNYYhz0HSG707I2SEBCo=", "signature": "px1wlarjFmFq23BLMntN3KyLlAgMfSxeIOXruAlQkucMASdcMx8QjOUBjR7gD64NFto3Nnax5YORFDi0sA1GAA==", "signed_message": "eW91cmRvbWFpbi5jb20gd2FudHMgeW91IHRvIHNpZ24gaW4gd2l0aCB5b3VyIFNvbGFuYSBhY2NvdW50Ogo5Z0JNQkRtdUREVVRTZGdpUTFTRzZ3RVprUDRYNUhGVnRZRFI1Q015dkZFeQoKU2lnbiBpbnRvIFJlYWN0IE5hdGl2ZSBTYW1wbGUgQXBwCgpVUkk6IGh0dHBzOi8vYml0cy5hcHAKTm9uY2U6IDEyc2pkYjc4NA"}}
-
-*/
 
 const styles = StyleSheet.create({
   container: {
--- app/solana/connectWallet.ts
+++ app/solana/connectWallet.ts
@@ -13,57 +13,87 @@ export const APP_IDENTITY = {
     uri: "https://bits.app",
     icon: "favicon.ico",
 };
+import { Buffer } from 'buffer';
 
-// export async function connectWallet() {
-//     return transact(async (wallet: Web3MobileWallet) => {
-//         const authorizationResult = await wallet.authorize({
-//             cluster: "devnet",
-//             identity: APP_IDENTITY,
-//         });
-//         console.log(authorizationResult);
-//         return authorizationResult;
-//     });
-// }
+window.Buffer = Buffer;
+
+export async function connectWallet() {
+    return transact(async (wallet: Web3MobileWallet) => {
+        const authorizationResult = await wallet.authorize({
+            cluster: "devnet",
+            identity: APP_IDENTITY,
+        });
+        console.log(authorizationResult.accounts[0].address);
+        return authorizationResult;
+    });
+}
 
 const SIGN_IN_PAYLOAD = {
-    domain: 'yourdomain.com',
+    domain: 'bits.app',
     nonce: "12sjdb784",
-    statement: 'Sign into React Native Sample App',
+    statement: 'Sign into Bits App to verfiy yourself',
     uri: 'https://bits.app',
+}
+
+function stringToUint8Arry(msg: string): Uint8Array {
+    const buffer = Buffer.from(msg, 'base64');
+    const uint8array = new Uint8Array(buffer);
+    console.log(uint8array);
+    return uint8array;
 }
 
 export async function signInNonce() {
     return transact(async (wallet: Web3MobileWallet) => {
-        console.log("hi");
-        
-        const authorizationResult = await wallet.authorize({
-            chain: 'solana:devnet',
-            identity: APP_IDENTITY,
-            sign_in_payload: SIGN_IN_PAYLOAD
-        });
-        console.log("sign in nonce result", authorizationResult);
-        // const ouptput = {
-        //      account: authorizationResult.accounts,
-        //      signedMessage: authorizationResult.sign_in_result?.signed_message,
-        //      signature: authorizationResult.sign_in_result?.signature,
-        // }
-        // const verifySIWSResult = verifySIWS(SIGN_IN_PAYLOAD,ouptput)
-        return authorizationResult.sign_in_result;
+        try {
+            const authorizationResult = await wallet.authorize({
+                chain: 'solana:devnet',
+                identity: APP_IDENTITY,
+                sign_in_payload: SIGN_IN_PAYLOAD
+            });
+
+            if (!authorizationResult.sign_in_result) {
+                throw new Error("no sign_in_result returned from wallet")
+            }
+            console.log("before SIGN_IN_OUTPUT parse ");
+
+            const SIGN_IN_OUTPUT: SolanaSignInOutput = {
+                account: {
+                    address: authorizationResult.accounts[0].address,
+                    publicKey: Object.freeze(stringToUint8Arry(authorizationResult.accounts[0].address)),
+                    chains: ["solana:devnet"],
+                    features: ["solana:signIn"]
+
+                },
+                signedMessage: stringToUint8Arry(authorizationResult.sign_in_result?.signed_message),
+                signature: stringToUint8Arry(authorizationResult.sign_in_result?.signature),
+            }
+            console.log("after SIGN_IN_OUTPUT parse ");
+
+            const verifySIWSResult = verifySIWS(SIGN_IN_PAYLOAD, SIGN_IN_OUTPUT)
+            console.log(verifySIWSResult);
+
+            return {
+                // signIn: authorizationResult.sign_in_result,
+                verfied: verifySIWSResult
+            };
+        } catch (e: any) {
+            console.log(e);
+        }
     })
 }
 
 
-// function verifySIWS(
-//     input: SolanaSignInInput,
-//     output: SolanaSignInOutput
-// ): boolean {
-//     const serialisedOutput: SolanaSignInOutput = {
-//         account: {
-//             publicKey: new Uint8Array(output.account.publicKey),
-//             ...output.account,
-//         },
-//         signature: new Uint8Array(output.signature),
-//         signedMessage: new Uint8Array(output.signedMessage),
-//     }
-//     return verifySignIn(input, serialisedOutput);
-// }
+function verifySIWS(
+    input: SolanaSignInInput,
+    output: SolanaSignInOutput
+): boolean {
+    const serialisedOutput: SolanaSignInOutput = {
+        account: {
+            ...output.account,
+            publicKey: new Uint8Array(output.account.publicKey),
+        },
+        signature: new Uint8Array(output.signature),
+        signedMessage: new Uint8Array(output.signedMessage),
+    }
+    return verifySignIn(input, serialisedOutput);
+}
