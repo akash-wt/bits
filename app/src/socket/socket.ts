f1bf76fdd48faa42fee5455de0c4b1373ddfcbdf
akash patel <akash906129@gmail.com>
2026-03-09 01:12:08 +0530

    added readme.md

--- app/src/socket/socket.ts
+++ app/src/socket/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://10.0.2.2:3000", {
+export const socket = io("https://bits.akashwt.org", {
     transports: ["websocket"],
 }); 
